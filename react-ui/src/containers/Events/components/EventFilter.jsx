3336a9528730b64c366d044a00cb9afc14c7e9c9
Adil Asif <adil@adilasif.com>
2018-12-11 13:32:11 -0500

    Add comments and remove unused imports

--- react-ui/src/containers/Events/components/EventFilter.jsx
+++ react-ui/src/containers/Events/components/EventFilter.jsx
@@ -1,25 +1,32 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
 import { Menu, Dropdown } from 'semantic-ui-react';
 
 const EventFilter = ({ handleItemClick, handleChange, categories }) => {
+  /**
+   * Initializes menu options with empty placeholder; placeholder doubles as
+   * selection to clear filter category selection
+   */
   let options = [
     {
       key: '-1', text: 'Select Category', value: '', content: 'Select Category',
     },
   ];
-  const categoryOptions = categories
+
+  // Generates rest of list from saved categories
+  const categoryList = categories
     && categories.map((category, index) => ({
       key: `${category}${index}`,
       text: category,
       value: category,
       content: category,
     }));
-  options = options.concat(categoryOptions);
+
+  // Adds generated category list to menu options array
+  options = options.concat(categoryList);
+
   return (
     <Fragment>
       <Menu.Item header>Filter By</Menu.Item>
-
       <Menu.Item name="filterByCategory">
         <Dropdown
           selection
@@ -36,7 +43,5 @@ const EventFilter = ({ handleItemClick, handleChange, categories }) => {
     </Fragment>
   );
 };
-
-EventFilter.propTypes = {};
 
 export default EventFilter;
--- react-ui/src/containers/Events/components/EventList.jsx
+++ react-ui/src/containers/Events/components/EventList.jsx
@@ -9,20 +9,24 @@ import EventFilter from './EventFilter';
 
 class EventList extends Component {
   componentDidMount = () => {
-    // this.props.fetchEvents();
+    // TODO: set this.props.fetchEvents() when API is linked
   };
 
+  // Allows user to filter events based on categories listed on page
   handleChange = (e, { value }) => {
     if (!value || value === '') this.props.setFilter('');
 
     this.props.setFilter(value);
   };
 
+  /**
+   * Shows placeholder text if no events are loaded, otherwise generates event
+   * list
+   */
   renderEvents = (events) => {
     if (!events) {
       return <h1>Loading Events</h1>;
     }
-
     return events.map(event => (
       <Grid.Column key={event.id} width={4}>
         <Event event={event} />
@@ -64,6 +68,15 @@ class EventList extends Component {
   }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  const { selectedCategory } = state.events;
+  return {
+    events: selectors.getFilteredEvents(state.events),
+    categories: state.events.categories,
+    selectedCategory,
+  };
+};
+
 const mapDispatchToProps = (dispatch, ownProps) => ({
   fetchEvents: () => {
     dispatch(eventActions.fetchEvents());
@@ -72,15 +85,5 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
     dispatch(eventActions.setFilter(filter));
   },
 });
-
-const mapStateToProps = (state, ownProps) => {
-  const { selectedCategory } = state.events;
-
-  return {
-    events: selectors.getFilteredEvents(state.events),
-    categories: state.events.categories,
-    selectedCategory,
-  };
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventList);
