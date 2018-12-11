60590ac68ed9bc73ceac6f49239b14421ea8a12e
Lasse Larsen <justlasse@gmail.com>
2018-12-11 18:47:32 +0100

    Bugfix/event filter no selection (#23)
    * Added animated buttons to homepage and changed some font sizes
    
    * Added simple event filtering with state and dropdown category menu
    
    * Added basic searchable filter to event Filter
    
    * Changed events filter to use redux
    
    * Added tests for redux
    
    * Added empty option to remove filter selection on homepage events list

--- react-ui/src/containers/Events/components/EventFilter.jsx
+++ react-ui/src/containers/Events/components/EventFilter.jsx
@@ -3,34 +3,38 @@ import PropTypes from 'prop-types';
 import { Menu, Dropdown } from 'semantic-ui-react';
 
 const EventFilter = ({ handleItemClick, handleChange, categories }) => {
-	const options =
-		categories &&
-		categories.map((category, index) => ({
-			key: `${category}${index}`,
-			text: category,
-			value: category,
-			content: category
-		}));
-	return (
-		<Fragment>
-			<Menu.Item header>Filter By</Menu.Item>
+  let options = [
+    {
+      key: '-1', text: 'Select Category', value: '', content: 'Select Category',
+    },
+  ];
+  const categoryOptions = categories
+    && categories.map((category, index) => ({
+      key: `${category}${index}`,
+      text: category,
+      value: category,
+      content: category,
+    }));
+  options = options.concat(categoryOptions);
+  return (
+    <Fragment>
+      <Menu.Item header>Filter By</Menu.Item>
 
-			<Menu.Item name="filterByCategory">
-				<Dropdown
-					placeholder="Category"
-					search
-					selection
-					header="Category"
-					basic
-					className="link item"
-					onChange={handleChange}
-					options={options}
-				/>
-			</Menu.Item>
-			<Menu.Item name="closest" onClick={handleItemClick} />
-			<Menu.Item name="mostPopular" onClick={handleItemClick} />
-		</Fragment>
-	);
+      <Menu.Item name="filterByCategory">
+        <Dropdown
+          selection
+          placeholder="Category"
+          search
+          basic
+          className="link item"
+          onChange={handleChange}
+          options={options}
+        />
+      </Menu.Item>
+      <Menu.Item name="closest" onClick={handleItemClick} />
+      <Menu.Item name="mostPopular" onClick={handleItemClick} />
+    </Fragment>
+  );
 };
 
 EventFilter.propTypes = {};
--- react-ui/src/containers/Events/components/EventList.jsx
+++ react-ui/src/containers/Events/components/EventList.jsx
@@ -1,82 +1,86 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Grid, Container, Header, Segment, Menu, Card } from 'semantic-ui-react';
+import {
+  Grid, Container, Header, Segment, Menu,
+} from 'semantic-ui-react';
 import { actions as eventActions, selectors } from '../../../store/events';
 import Event from './Event';
 import EventFilter from './EventFilter';
 
 class EventList extends Component {
-	componentDidMount = () => {
-		// this.props.fetchEvents();
-	};
+  componentDidMount = () => {
+    // this.props.fetchEvents();
+  };
 
-	handleChange = (e, { value }) => {
-		this.props.setFilter(value);
-	};
+  handleChange = (e, { value }) => {
+    if (!value || value === '') this.props.setFilter('');
 
-	renderEvents = events => {
-		if (!events) {
-			return <h1>Loading Events</h1>;
-		}
+    this.props.setFilter(value);
+  };
 
-		return events.map(event => <Event event={event} />);
-	};
+  renderEvents = (events) => {
+    if (!events) {
+      return <h1>Loading Events</h1>;
+    }
 
-	render() {
-		const { events, categories, selectedCategory } = this.props;
+    return events.map(event => (
+      <Grid.Column key={event.id} width={4}>
+        <Event event={event} />
+      </Grid.Column>
+    ));
+  };
 
-		return (
-			<Segment basic padded>
-				<Container>
-					<div
-						className="event-list-header"
-						style={{
-							display: 'flex',
-							justifyContent: 'space-between',
-							alignItems: 'center',
-							marginBottom: '1 em'
-						}}
-					>
-						<Menu borderless fluid style={{ border: 'none', boxShadow: 'none' }}>
-							<Menu.Item>
-								<Header as="h2">Events for this week</Header>
-							</Menu.Item>
-							<Menu.Item position="right">
-								<EventFilter
-									handleChange={this.handleChange}
-									categories={categories}
-								/>
-							</Menu.Item>
-						</Menu>
-					</div>
-					<Card.Group doubling stackable itemsPerRow={4}>
-						{this.renderEvents(events, selectedCategory)}
-					</Card.Group>
-				</Container>
-			</Segment>
-		);
-	}
+  render() {
+    const { events, categories, selectedCategory } = this.props;
+
+    return (
+      <Segment basic padded>
+        <Container>
+          <div
+            className="event-list-header"
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginBottom: '1 em',
+            }}
+          >
+            <Menu borderless fluid style={{ border: 'none', boxShadow: 'none' }}>
+              <Menu.Item>
+                <Header as="h2">Events for this week</Header>
+              </Menu.Item>
+              <Menu.Item position="right">
+                <EventFilter
+                  handleChange={this.handleChange}
+                  categories={categories}
+                />
+              </Menu.Item>
+            </Menu>
+          </div>
+          <Grid columns={4}>{this.renderEvents(events, selectedCategory)}</Grid>
+        </Container>
+      </Segment>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-	...bindActionCreators(
-		{
-			fetchEvents: eventActions.fetchEvents,
-			setFilter: eventActions.setFilter
-		},
-		dispatch
-	)
+  fetchEvents: () => {
+    dispatch(eventActions.fetchEvents());
+  },
+  setFilter: (filter) => {
+    dispatch(eventActions.setFilter(filter));
+  },
 });
 
 const mapStateToProps = (state, ownProps) => {
-	const { selectedCategory } = state.events;
+  const { selectedCategory } = state.events;
 
-	return {
-		events: selectors.getFilteredEvents(state.events),
-		categories: state.events.categories,
-		selectedCategory: selectedCategory
-	};
+  return {
+    events: selectors.getFilteredEvents(state.events),
+    categories: state.events.categories,
+    selectedCategory,
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventList);
