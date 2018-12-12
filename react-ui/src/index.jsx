2ddd3d47104d70700fa1dd4f67cb9dfde8863fc2
Adil Asif <adil@adilasif.com>
2018-12-12 11:08:12 -0500

    Fix merge conflict

--- react-ui/src/index.jsx
+++ react-ui/src/index.jsx
@@ -22,16 +22,16 @@ import SignIn from './components/Auth/SignIn';
 const target = document.getElementById('root');
 
 render(
-	<Provider store={store}>
-		<ConnectedRouter history={reduxHistory}>
-			<App>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/signup" component={Signup} />
-				<Route path="/signin" component={SignIn} />
-			</App>
-		</ConnectedRouter>
-	</Provider>,
-	target
+  <Provider store={store}>
+    <ConnectedRouter history={reduxHistory}>
+      <App>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={SignIn} />
+      </App>
+    </ConnectedRouter>
+  </Provider>,
+  target
 );
 
 // If you want your app to work offline and load faster, you can change
