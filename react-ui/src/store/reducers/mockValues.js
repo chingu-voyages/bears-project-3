d08c89a372add6e15b70c9b8e273541f7169557b
Lasse Larsen <justlasse@gmail.com>
2018-12-16 13:26:42 +0100

    Added all mock values as per zen hub Event creation issue.

--- react-ui/src/store/reducers/mockValues.js
+++ react-ui/src/store/reducers/mockValues.js
@@ -47,9 +47,23 @@ export const generatedEventsArray = Array.from({ length: 30 }, (v, i) => ({
 	title: `Game Session ${i}`,
 	category: eventCategories[Math.floor(Math.random() * eventCategories.length)],
 	description: 'Event Description',
+	details: [],
+	photos: [
+		'https://react.semantic-ui.com/images/avatar/large/matthew.png',
+		'https://react.semantic-ui.com/images/avatar/large/matthew.png'
+	],
+	location: {
+		address: '123 Somekindofway',
+		city: 'Los Angeles',
+		state: 'CA',
+		country: 'USA'
+	},
 	color: colors[Math.floor(Math.random() * colors.length)],
 	dateStarting: generateFutureDate(new Date()),
 	timeStarting: new Date(),
+	timeEnding: new Date(),
+	inviteCode: 'EVENTCODE123',
+	isPublic: true,
 	attendees: [],
 	mainImage: 'https://react.semantic-ui.com/images/avatar/large/matthew.png',
 	creator: {
