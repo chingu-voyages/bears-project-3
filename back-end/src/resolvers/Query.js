727a507509d253e0b761c9ce5c29360604b29177
Adil Asif <asifadilanis@gmail.com>
2018-12-20 18:44:12 -0500

    Removed unused queries from resolver

--- back-end/src/resolvers/Query.js
+++ back-end/src/resolvers/Query.js
@@ -1,13 +1,5 @@
 const Query = {
-  async events(parent, args, context, info) {
-    const events = await context.prisma.query.events();
-    return events;
-  },
 
-  async users(parent, args, context, info) {
-    const users = await context.prisma.query.users();
-    return users;
-  }
 };
 
 module.exports = Query;
