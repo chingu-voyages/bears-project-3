a0eafc50d2fed59edcbae7912986bda84f998701
Lasse Larsen <>
2019-01-02 12:56:49 +0100

    Added formik and validation to signup form

--- front-end/src/containers/Auth/Register.jsx
+++ front-end/src/containers/Auth/Register.jsx
@@ -1,77 +1,87 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Mutation } from 'react-apollo';
 import { Link } from 'react-router-dom';
+import { withFormik } from 'formik'
 import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
 
 import { AUTH_TOKEN } from '../../utils/constants';
 import { REGISTER_USER } from '../../utils/mutations';
 
-class Register extends Component {
-  state = {
-    email: '',
-    password: '',
-    name: '',
-  }
-
-  confirm = async (data) => {
+const Register = ({ values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur, history, ...props
+}) => {
+  const confirm = async (data) => {
     const { token } = data.register;
-    this.saveUserData(token);
-    this.props.history.push('/');
+    saveUserData(token);
+    history.push('/');
   }
 
   // TODO: Remove local storage and implement session cookie
-  saveUserData = (token) => {
+  const saveUserData = (token) => {
     localStorage.setItem(AUTH_TOKEN, token);
   }
 
-  render() {
-    const { email, password, name } = this.state;
-    return (
-      <Segment basic padded className="signin-form">
-        <Grid textAlign="center" style={{ height: '79vh' }} verticalAlign="middle">
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h2" color="purple" textAlign="center">
-              Register an account
+  const { name, email, password } = values
+  const { name: nameError, email: emailError, password: passwordError } = errors
+
+  return (
+    <Segment basic padded className="signin-form">
+      <Grid textAlign="center" style={{ height: '79vh' }} verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" color="purple" textAlign="center">
+            Register an account
             </Header>
-            <Form size="large">
-              <Segment stacked>
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="Name"
-                  value={name}
-                  onChange={e => this.setState({ name: e.target.value })}
-                  type="text"
-                />
-                <Form.Input
-                  fluid
-                  icon="envelope"
-                  iconPosition="left"
-                  type="text"
-                  placeholder="E-mail"
-                  autoComplete="email"
-                  value={email}
-                  onChange={e => this.setState({ email: e.target.value })}
-                />
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  type="password"
-                  placeholder="Password"
-                  autoComplete="off"
-                  value={password}
-                  onChange={e => this.setState({ password: e.target.value })}
-                />
-              </Segment>
-              <Mutation
-                mutation={REGISTER_USER}
-                variables={{ email, password, name }}
-                onCompleted={data => this.confirm(data)}
-              >
-                {mutation => (
+          <Mutation
+            mutation={REGISTER_USER}
+            variables={{ email, password, name }}
+            onCompleted={data => confirm(data)}
+          >
+            {mutation => (
+              <Form size="large" onSubmit={mutation}>
+                <Segment stacked>
+
+                  <Form.Input
+                    error={errors.name && touched ? true : false}
+                    onBlur={handleBlur}
+                    icon="user"
+                    iconPosition="left"
+                    placeholder="Name"
+                    value={name}
+                    type="text"
+                    name='name'
+                    onChange={handleChange}
+                  />
+                  <Form.Input
+                    error={errors.email && touched ? true : false}
+                    onBlur={handleBlur}
+                    icon="envelope"
+                    iconPosition="left"
+                    type="text"
+                    placeholder="E-mail"
+                    autoComplete="email"
+                    value={email}
+                    onChange={handleChange}
+                    name='email'
+                  />
+                  <Form.Input
+                    error={errors.password && touched ? true : false}
+                    onBlur={handleBlur}
+                    icon="lock"
+                    iconPosition="left"
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={handleChange}
+                    name='password'
+                  />
+
+                  {errors.name && touched.name && <div id="feedback">{errors.name}</div>}
                   <Button
+                    disabled={nameError || emailError || passwordError}
                     fluid
                     maxWidth="50%"
                     color="purple"
@@ -80,19 +90,48 @@ class Register extends Component {
                   >
                     Register
                   </Button>
-                )}
-              </Mutation>
-            </Form>
-            <Link to='/signin'>
-              <Button style={{ margin: 10 }}>
-                Already have an account?
+                </Segment>
+              </Form>
+            )}
+          </Mutation>
+          <Link to='/signin'>
+            <Button style={{ margin: 10 }}>
+              Already have an account?
             </Button>
-            </Link>
-          </Grid.Column>
-        </Grid>
-      </Segment>
-    );
-  }
+          </Link>
+        </Grid.Column>
+      </Grid>
+    </Segment >
+  );
 }
 
-export default Register;
+const FormikSignup = withFormik({
+  mapPropsToValues: () => ({ name: '', email: '', password: '' }),
+
+  // Sync Validation
+  validate: values => {
+    const errors = {};
+
+    errors.name = !values.name
+      ? 'Name is Required'
+      : !values.name.match(/[A-Za-z]/g)
+        ? 'Name can only be letters'
+        : undefined
+
+    errors.email = !values.email
+      ? 'Email is Required'
+      : !values.email.match(/@/g)
+        ? 'Email must be valid'
+        : undefined
+
+    errors.password = !values.password
+      ? 'Password is Required'
+      : undefined
+
+
+    return errors;
+  },
+  displayName: 'SignupForm',
+})(Register)
+
+export default FormikSignup;
